0c22b35990d5a24718fc8cc4dfa49224502947ad
Damian <100926905+DamianHulsman@users.noreply.github.com>
2023-10-31 12:28:06 +0100

    begin regio bepalen

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,16 @@
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { useState } from 'react';
+import axios from 'axios';
 import * as Location from 'expo-location';
 import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
     const [lat, setlat] = useState(0.0);
     const [lon, setlon] = useState(0.0);
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
 
     const getGeolocation = async () => {
-        try {
-
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
                 setErrorMsg('Permission to access location was denied');
@@ -19,16 +20,31 @@ const Map = () => {
             let location = await Location.getCurrentPositionAsync({});
             setlat(location.coords.latitude);
             setlon(location.coords.longitude);
-        } catch (err) {
-            console.log(err);
+            const response = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${location.coords.latitude}&lon=${location.coords.longitude}&limit=5&appid=f25e31f2eaa6e1b215ca709bf964d2db`);
+            setCity(response.data[0].name);    
+            setState(response.data[0].state);
+            console.log(response.data);
+    }
+
+    const getRegion = () => {
+        if (state === 'Drenthe' || 'Flevoland' || 'Friesland' || 'Groningen' || 'Noord-Holland' || 'Overijssel' && city !== 'Zeewolde') {
+            alert('Regio = Noord');
+        } else if (state === 'Zuid-Holland' || city === 'Zeewolde') {
+            alert('Regio = Midden');
+        } else if (state === 'Limburg' || 'Zeeland') {
+            alert('Regio = Zuid');
         }
     }
-    getGeolocation();
+    // getGeolocation();
     return (
         <View style={styles.container}>
+            <Button onPress={getGeolocation} title="Getgeolocation" color={'green'}/>
+            <Button onPress={getRegion} title="Get Region" color={'lightgreen'} />
             <Text style={styles.title}>Kaart</Text>
             <Text>Latitude: {lat}</Text>
             <Text>Longitude: {lon}</Text>
+            <Text>City: {city}</Text>
+            <Text>State: {state}</Text>
             <MapView
             scrollEnabled={false}
             zoomEnabled={false}
